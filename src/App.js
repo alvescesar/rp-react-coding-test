239de17394c9bee3dde832e70d413bff8b52815d
César Alves <alvescesar@hotmail.com>
2022-06-24 15:32:25 -0300

    Refactoring initial fetch and adding next table button

--- src/App.js
+++ src/App.js
@@ -10,12 +10,40 @@ class App extends Component {
     table: tableShape.isRequired,
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      table: {},
+      tableId: 1
+    };
+  }
+
+  async componentWillMount() {
+    const response = await fetch(`https://storage.googleapis.com/replaypoker-dummy-api/tables/${this.state.tableId}.json`);
+    const table = await response.json();
+    this.setState({ table });
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.tableId !== this.state.tableId) {
+      this.componentWillMount();
+    }
+  }
+
+  nextTable() {
+    const { tableId } = this.state;
+
+    if (tableId === 8) return this.setState({ tableId: 1 });
+    this.setState({ tableId: tableId + 1 });
+  }
+
   render() {
-    const { table } = this.props;
+    const { table } = this.state;
 
     return (
       <div className="App">
         <Table table={table} />
+        <button type="button" onClick={() => this.nextTable()}>Next Table</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// TODO: Retrieve this data from the API rather than a fixture
-// import table from './data/table-1'
-
-const tableId = 1;
-
-fetch(`https://storage.googleapis.com/replaypoker-dummy-api/tables/${tableId}.json`)
-  .then(response => response.json())
-  .then(table => {
-    console.log("TABLE HERE => ", table);
-    ReactDOM.render(<App table={table} />, document.getElementById('root'));
-  })
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
